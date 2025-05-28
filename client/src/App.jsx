6fefac8b2fe54f64ebc383ad13188f23033f498a
nikhilraj021 <dnraj2001@gmail.com>
2025-05-28 22:15:14 +0530

    updated favicon image and added google font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='text-center'>
-      <h1 className='text-2xl'></h1>
+      <h1 className='text-2xl'>Hello</h1>
     </div>
   )
 }
