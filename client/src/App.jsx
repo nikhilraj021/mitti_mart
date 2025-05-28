71b6ff1771ea08a83a644c9bb47a704a7846ab8d
nikhilraj021 <dnraj2001@gmail.com>
2025-05-28 22:27:46 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className='text-center'>
-      <h1 className='text-2xl'>Hello</h1>
+      <h1 className='text-2xl font-poppins text-primary'>Hello</h1>
+      <h1 className='text-2xl text-primary-dull text-shadow-lg text-shadow-black'>Hello</h1>
+
     </div>
   )
 }
